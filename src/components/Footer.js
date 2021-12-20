288e030970a6fae4a9fc0893631a4d7fd4901cb2
elfgod <albertogx@icloud.com>
2021-12-19 23:42:47 -0500

    Updating Footer and Erro Page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      FOOTER
-    </div>
+    <footer className="page-footer">
+      <p>&copy; {" "}{new Date().getFullYear()}<span>PropinVC</span>
+      . Built by{" "}<a href="https://alienroom.dev">AlienRoom</a>
+      </p>
+    </footer>
   )
 }
 
