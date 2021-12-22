58455bb553199f6b4ece1921ff78c1c42438823b
elfgod <albertogx@icloud.com>
2021-12-21 19:56:15 -0500

    Contact page, Navbar setup, Navbar logic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
+import { FiAlignJustify } from 'react-icons/fi'
+import logo from '../assets/images/navbar-propinvc.png'
 
 const Navbar = () => {
+  const [show, setShow] = useState(false)
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to='/'>Inicio</Link>
-        </li>
-        <li>
-          <Link to='/alquiler'>Alquiler</Link>
-        </li>
-        <li>
-          <Link to='/venta'>Venta</Link>
-        </li>
-        <li>
-          <Link to='/servicios'>Servicios</Link>
-        </li>
-        <li>
-          <Link to='/contacto'>Contacto</Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+        <Link to="/">
+          <img src={logo} alt="Encuentra las mejores propiedades" />
+        </Link>
+        <button className="nav-btn" onClick={() => setShow(false)}>
+          <FiAlignJustify />
+        </button>
+        </div>
+        <div className={
+          show ? "nav-links show-links" : "nav-links"
+        }>
+          <Link to="/" className="nav-link"
+          activeClassName="active-link"
+          onClick={() => setShow(false)}
+          >
+            inicio
+          </Link>
+          <Link to="/alquiler" className="nav-link"
+          activeClassName="active-link"
+          onClick={() => setShow(false)}
+          >
+            alquiler
+          </Link>
+          <Link to="/venta" className="nav-link"
+          activeClassName="active-link"
+          onClick={() => setShow(false)}
+          >
+            venta
+          </Link>
+          <Link to="/servicios" className="nav-link"
+          activeClassName="active-link"
+          onClick={() => setShow(false)}
+          >
+            servicios
+          </Link>
+          <div className="nav-link contact-link">
+          <Link to="/contact" className="btn"
+          onClick={() => setShow(false)}
+          >
+              contacto
+            </Link>
+          </div>
+        </div>
+      </div>     
     </nav>
   )
 }
--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -4,7 +4,45 @@ import Layout from '../components/Layout'
 const contacto = () => {
   return (
     <Layout>
-      <h3>Contacto</h3>
+      <main className="page">
+        <section className="contact-page">
+          <article className="contact-info">
+            <h3>Contatanos</h3>
+            <p>
+              Para mas informacion sobre como publicar tu propiedades, ya sea para
+              venta o renta.
+            </p>
+            <p>Para informacion sobre cotizaciones de como podemos ayudarte
+              en la administracion de tu propiedad.
+            </p>
+            <p>Para mas informacion sobre como podemos ayudarte con las reparaciones
+              y mejoras a tu propiedad.
+            </p>
+            <p>O simplemente cualquier consulta que tengas pensado estamos abiertos
+              a escucharte gracias.
+            </p>
+          </article>
+          <article>
+            <form className="form contact-form">
+              <div className="form-row">
+                <label htmlFor="name">Your name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="email">Your email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="message">message</label>
+                <textarea name="message" id="message"></textarea>
+              </div>
+              <button type="submit" className="btn block">
+                submit
+              </button>
+            </form>
+          </article>
+        </section>
+      </main>
     </Layout>
   )
 }
